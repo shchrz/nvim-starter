2646f997dfc71d4f794a5487278118ff3bf470e0
Heiker Curiel <heikercuriel@gmail.com>
2022-09-04 17:32:24 -0400

    add init.lua

---
+++ init.lua
@@ -0,0 +1,108 @@
+vim.api.nvim_create_user_command('GitClonePacker', function()
+  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  local packer_url = 'https://github.com/wbthomason/packer.nvim'
+  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+  vim.cmd('quitall')
+end, {})
+
+local ok, packer = pcall(require, 'packer')
+if not ok then return end
+
+packer.startup(function(use)
+  use {'wbthomason/packer.nvim'}       -- Plugin manager
+  use {'joshdick/onedark.vim'}         -- Colorscheme
+  use {'neovim/nvim-lspconfig'}        -- LSP support
+  use {'hrsh7th/nvim-cmp'}             -- Autocomplete engine
+  use {'hrsh7th/cmp-nvim-lsp'}         -- Completion source
+  use {'saadparwaiz1/cmp_luasnip'}     -- Completion source
+  use {'hrsh7th/cmp-buffer'}           -- Completion source
+  use {'L3MON4D3/LuaSnip'}             -- Snippet engine
+  use {'rafamadriz/friendly-snippets'} -- Snippet collection
+end)
+
+vim.opt.termguicolors = true
+vim.cmd('colorscheme onedark')
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  sources = cmp.config.sources({
+    {name = 'nvim_lsp'},
+    {name = 'luasnip'},
+  },{
+    {name = 'buffer'},
+  }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<Enter>'] = cmp.mapping.confirm({select = false}),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+  }),
+})
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LspAttached',
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      vim.keymap.set(mode, lhs, rhs, {buffer = true})
+    end
+
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+    bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+  end
+})
+
+local lspconfig = require('lspconfig')
+local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = require('cmp_nvim_lsp').update_capabilities(
+  vim.lsp.protocol.make_client_capabilities()
+)
+
+lsp_defaults.on_attach = function()
+  vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
+end
+
+lspconfig.html.setup({})
+lspconfig.cssls.setup({})
+lspconfig.eslint.setup({})
+lspconfig.tsserver.setup({})
+
